631fc8d0fba7bc54982ab86e2066bef7140f4040
Adriano <124369848+agzzzz@users.noreply.github.com>
2023-11-12 06:52:43 -0300

    Add to github

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -7,6 +10,10 @@
 #include "configuration.h"
 #include "shiftOut.h"
 #include "kinematics.h"
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+
+#define MOUNT_POINT "/sd"
 
 static const char *TAG = "main";
 
@@ -19,7 +26,89 @@ void app_main()
     esp_err_t ret;
     vTaskDelay(1500/portTICK_PERIOD_MS);
 
+    // Options for mounting the filesystem.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = true,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024,
+        .disk_status_check_enable = false
+    };
+    sdmmc_card_t *card;
+    const char mount_point[] = MOUNT_POINT;
+    ESP_LOGI(TAG, "Initializing SD card");
+
+    ESP_LOGI(TAG, "Using SPI peripheral");
+
+    // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT (20MHz)
+    // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 20MHz for SDSPI)
+    // Example: for fixed frequency of 10MHz, use host.max_freq_khz = 10000;
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+    host.set_card_clk(host.slot, 1000);
+
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = SD_MOSI_PIN,
+        .miso_io_num = SD_MISO_PIN,
+        .sclk_io_num = SD_SCLK_PIN,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4000,
+    };
+    ret = spi_bus_initialize((spi_host_device_t)host.slot, &bus_cfg, SDSPI_DEFAULT_DMA);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize bus.");
+        return;
+    }
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = SD_CS_PIN;
+    slot_config.host_id = (spi_host_device_t)host.slot;
+
+    ESP_LOGI(TAG, "Mounting filesystem");
+    ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount filesystem. "
+                     "If you want the card to be formatted, set the CONFIG_EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+    ESP_LOGI(TAG, "Filesystem mounted");
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    std::ifstream thrFile("/sd/stars.thr", std::ifstream::in);
+
+    if (!thrFile.is_open()) {
+        ESP_LOGE(TAG, "Error opening file");
+        ESP_ERROR_CHECK(ESP_ERR_INVALID_STATE);
+    }
+    ESP_LOGI(TAG, "File opened");
+
     kinematicsSetup();
     home();
     
+    char line[64];
+    while (thrFile.good()) {
+        thrFile.getline(line, 64);
+        char* strThetaEnd;
+        char* strRhoEnd;
+        float theta, rho;
+
+        theta = strtof(line, &strThetaEnd);
+        rho = strtof(strThetaEnd, &strRhoEnd);
+        if (strThetaEnd != line && strRhoEnd != strThetaEnd) {
+            ESP_LOGI(TAG, "THR = (%f, %f)", theta, rho);
+            move(theta, rho, 10.0);
+        }
+        else {
+            ESP_LOGW(TAG, "Invalid conversion\n");
+        }
+    }
 }
